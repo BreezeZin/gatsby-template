4a5b182e85dcbf8685b3b16f2bf36ed32dbe5528
BreezeZin <7067090+BreezeZin@users.noreply.github.com>
2019-12-02 15:31:42 +1000

    Added postcss variables and loops

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = () => ({
   plugins: [
     require("postcss-import"),
+    require('postcss-advanced-variables'),
     require("postcss-nested"),
     require("tailwindcss"),
     require("autoprefixer"),
