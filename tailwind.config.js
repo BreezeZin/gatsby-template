ec187807c4ddd25daefa5c72b8fdea77287657c7
Jason Noble <jason345@gmail.com>
2019-12-05 19:58:06 +1000

    Default breakpoints and css utilities

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    screens: {
+      xs: "512px",
+      sm: "640px",
+      md: "768px",
+      lg: "896px",
+      xl: "1024px",
+      "2xl": "1280px"
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    container: false
+  }
+};
